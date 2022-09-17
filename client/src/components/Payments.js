466a3dc1b1a68662aaeed85a225f6139068adad3
omar2004712 <o.bahutair@gmail.com>
2022-09-17 12:19:59 +0400

    build: set up the frontend to request for adding credits for the user

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends Component {
   render() {
+    console.log(this);
+
     return (
       <StripeCheckout
         name="Emaily"
         description="$5 dollars for 5 email credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="btn">ADD CREDITS</button>
@@ -17,4 +21,4 @@ class Payments extends Component {
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
